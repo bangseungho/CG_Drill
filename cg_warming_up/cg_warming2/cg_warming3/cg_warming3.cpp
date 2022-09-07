ef42ff8c4ccea64bce88defbf8ca515e97f49437
bangseungho <suengho2257@naver.com>
2022-09-07 22:03:27 +0900

    cg_waming2 complete!

--- cg_warming_up/cg_warming2/cg_warming3/cg_warming3.cpp
+++ cg_warming_up/cg_warming2/cg_warming3/cg_warming3.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 using namespace std;
 
-void ReverseWord(char str[]);
-void PrintInfo(char str[]);
-void AddWord(char str[]);
+int CountWord(char str[]);
+int CountNum(char str[]);
+int CountCapital(char str[]);
 
 int main()
 {
@@ -21,80 +21,79 @@ int main()
     
     fclose(fp);
 
-    while (true)
-    {
-        cin >> input;
+    word_cnt = CountWord(str);
+    num_cnt = CountNum(str);
+    cap_cnt = CountCapital(str);
 
-        if (input == 'q')
-            break;
-
-        switch (input)
-        {
-        case 'd':
-            ReverseWord(str);
-            break;
-        case 'e':
-            AddWord(str);
-            break;
-        case 'f':
-            break;
-        case 'g':
-            break;
-        case 'h':
-            break;
-        }
-
-    }
+    cout << "word count : " << word_cnt << endl;
+    cout << "number count : " << num_cnt << endl;
+    cout << "capital count : " << cap_cnt << endl;
 
     return 0;
 }
 
-void ReverseWord(char str[])
+int CountWord(char str[])
 {
-    char temp[MAX] = {};
-    
-    int len = strlen(str);
+    int ret = 0;
+    int i = 0;
 
-    for (int i = 0; i<= len; i++)
+    while (true)
     {
-        temp[i] = str[strlen(str) - i - 1];
-    }
-
-    for (int i = 0; i <= len; i++)
-    {
-        str[i] = temp[i];
-    }
-
-    str[len] = '\0';
-
-    PrintInfo(str);
-}
-
-void AddWord(char str[])
-{
-    int cnt = 0;
-
-    for (int i = 0; i <= strlen(str); i++)
-    {
-        if (cnt % 4 == 0)
+        i++;
+ 
+        if (str[i] == ' ' || str[i] == '.' || str[i] == '\n' || str[i] == '\0')
         {
-            str[i] = '@';
+            if (str[i - 1] >= 65 && str[i - 1] <= 90 ||
+                str[i - 1] >= 97 && str[i - 1] <= 122)
+            {
+                ret++;
+
+            }
         }
-        cnt++;
+
+        if (str[i] == '\0')
+            break;
     }
 
-    PrintInfo(str);
+    return ret;
 }
 
-void PrintInfo(char str[])
+int CountNum(char str[])
 {
-    int len = strlen(str);
+    int ret = 0;
+    int i = 0;
 
-    for (int i = 0; i <= len; i++)
+    while (true)
     {
-        cout << str[i];
+        i++;
+
+        if (str[i] == ' ' || str[i] == '.' || str[i] == '\n' || str[i] == '\0')
+        {
+            if (str[i - 1] >= 48 && str[i - 1] <= 57)
+            {
+                ret++;
+
+            }
+        }
+
+        if (str[i] == '\0')
+            break;
     }
 
-    cout << endl;
+    return ret;
 }
 
+int CountCapital(char str[])
+{
+    int ret = 0;
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i - 1] >= 65 && str[i - 1] <= 90)
+        {
+            ret++;
+        }
+    }
+
+    return ret;
+}
